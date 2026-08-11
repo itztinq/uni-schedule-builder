a1e4b70cf55f92143fe786ee8b849d040d30252f
Martina <tina.stojanovska7@gmail.com>
2026-08-11 21:12:14 +0200

    Initial commit: MVC project setup with EF Core and Identity packages

---
+++ uni-schedule-builder/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace uni_schedule_builder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
